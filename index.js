f200281f3ab75abd6eabc13cf2ff4ea4229f2bb7
Yanters <43695512+Yanters@users.noreply.github.com>
2020-01-18 16:59:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ let args = message.content.substring(PREFIX.length).split(" ");
 function image(message){
  
     var options = {
-        url: "http://results.dogpile.com/serp?qc=images&q=" + "cats",
+        url: "http://results.dogpile.com/serp?qc=images&q=" + "cursed images",
         method: "GET",
         headers: {
             "Accept": "text/html",
