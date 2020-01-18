3301edf4a84e3cb8b84e5ce44b3a895ddff0c767
Yanters <43695512+Yanters@users.noreply.github.com>
2020-01-18 17:11:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ let args = message.content.substring(PREFIX.length).split(" ");
                 message.channel.sendMessage(attachment);
 
         break;
-        case 'image':
+        case 'meme':
             image(message);
              break;
         case 'help':
@@ -109,7 +109,7 @@ let args = message.content.substring(PREFIX.length).split(" ");
 function image(message){
  
     var options = {
-        url: "http://results.dogpile.com/serp?qc=images&q=" + "meme",
+        url: "http://results.dogpile.com/serp?qc=images&q=" + "dark meme",
         method: "GET",
         headers: {
             "Accept": "text/html",
