adbda2f4f6ccdfd1b1ae7ce4248e011df1e22f3c
Yanters <43695512+Yanters@users.noreply.github.com>
2020-01-26 20:34:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ bot.on('ready', () =>{
 bot.on('message', message=>{
  
  let args = message.content.substring(PREFIX.length).split(" ");
- 
- if(message.content === "XD"||message.content === "xd"||message.content === "xD")
- {
-  message.channel.sendMessage('XDDD');
- }
+ let arg = message.content.toLowerCase();
+    if(arg[0]=== 'x' && arg[1]===[d])
+    {
+     message.channel.sendMessage('XDDD');
+    }
  
  switch(args[0]){
        case 'XD':
