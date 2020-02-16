f76abaf97d492fac2f3e6e47a09cabd434c38d4c
Yanters <43695512+Yanters@users.noreply.github.com>
2020-02-16 11:48:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -108,7 +108,6 @@ bot.on('message', message=>{
             }
             message.channel.bulkDelete(deleteAmount, true)
                 .then(deleted => message.channel.send(`I deleted \`${deleted.size}\` messages.`))
-                .then(deleted => message.channel.send(`I deleted \`${deleted.size}\` messages.`))
                 .then(m => m.delete(3000));
             break;                
     }
